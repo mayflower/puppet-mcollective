18a1829fcc85480ffe7526af0f533e0e28cb321e
Franz Pletz <fpletz@fnordicwalking.de>
2014-02-10 15:37:32 +0100

    Remove Debian/Ubuntu hacks, fixed upstream

--- manifests/package/server.pp
+++ manifests/package/server.pp
@@ -17,23 +17,6 @@ class mcollective::package::server {
   }
 
   case $::osfamily {
-    Debian: {
-      # Mcollective packages currently install into ruby/1.8 instead of vendor_ruby
-      # for compatibility with hardy. If the current rubyversion is 1.9 then we
-      # need symlinks so mcollective can find itself.
-      if $::rubyversion =~ /^1\.9/ {
-        file {
-          '/usr/lib/ruby/vendor_ruby/mcollective.rb':
-            ensure => link,
-            target => '/usr/lib/ruby/1.8/mcollective.rb',
-            before => Service[$mcollective::params::servicename];
-          '/usr/lib/ruby/vendor_ruby/mcollective':
-            ensure => link,
-            target => '/usr/lib/ruby/1.8/mcollective',
-            before => Service[$mcollective::params::servicename];
-        }
-      }
-    }
     FreeBSD: {
       # The mcollective package drops all plugins in /usr/local/share/mcollective
       # which breaks the mcollective libdir path, which expects "${libdir}/mcollective"
